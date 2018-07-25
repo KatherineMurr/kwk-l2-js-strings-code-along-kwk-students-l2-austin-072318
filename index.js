f82faf55d49df1fea444121cd63da650ae77119a
KatherineMurr <katherinemurr@yahoo.com>
2018-07-25 15:20:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ let currentUser = "Katherine Murr"
 let CURRENTUSER = "KATHERINE MURR"
 let welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
 let excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, " + CURRENTUSER + "!"
+let shortGreeting = "Welcome, " + currentUser
