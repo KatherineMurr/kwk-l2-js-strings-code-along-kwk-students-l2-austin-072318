be1ac01ef0e0acb49d1864d27e6c2cec1162f622
KatherineMurr <katherinemurr@yahoo.com>
2018-07-25 15:26:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let currentUser = "Katherine Murr"
 let welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
-let excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, " + currentUser.toUpperCase + "!"
+let excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, " + currentUser.toUpperCase() + "!"
 let shortGreeting = "Welcome, " + currentUser.CharAt(0) + "!"
